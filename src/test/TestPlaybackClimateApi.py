1e17f67f72affd9ed9ccae3fed59cbfde179d909
Paul Hammant <paul@hammant.org>
2020-12-26 21:23:21 +0000

    fix method names

--- src/test/TestPlaybackClimateApi.py
+++ src/test/TestPlaybackClimateApi.py
@@ -18,32 +18,32 @@ class TestPlaybackClimateApi(TestClimateApi):
 
     # mock_recordings = parser.get_recordings(os.path.dirname(os.path.realpath(__file__)).replace('main', 'mocks'))
 
-    MockService.set_markdown_files(MOCKS_DIR)
+    MockService.set_mocks_dir(MOCKS_DIR)
     thread1 = threading.Thread(target=MockService.start, daemon=True)
     thread1.start()
 
     def test_averageRainfallForGreatBritainFrom1980to1999Exists(self):
-        MockService.MockServiceHttpHandler.set_invoking_method(inspect.stack()[0][3])
+        MockService.MockServiceHttpHandler.set_markdown_filename(inspect.stack()[0][3])
         super().test_averageRainfallForGreatBritainFrom1980to1999Exists()
 
     def test_averageRainfallForFranceFrom1980to1999Exists(self):
-       MockService.MockServiceHttpHandler.set_invoking_method(inspect.stack()[0][3])
+       MockService.MockServiceHttpHandler.set_markdown_filename(inspect.stack()[0][3])
        super().test_averageRainfallForFranceFrom1980to1999Exists()
 
     def test_averageRainfallForEgyptFrom1980to1999Exists(self):
-        MockService.MockServiceHttpHandler.set_invoking_method(inspect.stack()[0][3])
+        MockService.MockServiceHttpHandler.set_markdown_filename(inspect.stack()[0][3])
         super().test_averageRainfallForEgyptFrom1980to1999Exists()
 
     def test_averageRainfallForGreatBritainFrom1985to1995DoesNotExist(self):
-        MockService.MockServiceHttpHandler.set_invoking_method(inspect.stack()[0][3])
+        MockService.MockServiceHttpHandler.set_markdown_filename(inspect.stack()[0][3])
         super().test_averageRainfallForGreatBritainFrom1985to1995DoesNotExist()
 
     def test_averageRainfallForMiddleEarthFrom1980to1999DoesNotExist(self):
-        MockService.MockServiceHttpHandler.set_invoking_method(inspect.stack()[0][3])
+        MockService.MockServiceHttpHandler.set_markdown_filename(inspect.stack()[0][3])
         super().test_averageRainfallForMiddleEarthFrom1980to1999DoesNotExist()
 
     def test_averageRainfallForGreatBritainAndFranceFrom1980to1999CanBeCalculatedFromTwoRequests(self):
-        MockService.MockServiceHttpHandler.set_invoking_method(inspect.stack()[0][3])
+        MockService.MockServiceHttpHandler.set_markdown_filename(inspect.stack()[0][3])
         super().test_averageRainfallForGreatBritainAndFranceFrom1980to1999CanBeCalculatedFromTwoRequests()
 
 
--- src/test/TestRecordingClimateApi.py
+++ src/test/TestRecordingClimateApi.py
@@ -12,7 +12,7 @@ import servirtium.recorder as MockRecorder
 class TestRecordingClimateApi(TestClimateApi):
     servirtium_site = "http://localhost:61417"
 
-    MockRecorder.set_markdown_files(MOCKS_DIR)
+    MockRecorder.set_mocks_dir(MOCKS_DIR)
     MockRecorder.set_real_service('http://climatedataapi.worldbank.org')
     MockRecorder.set_request_header_replacements({'User-Agent': 'Servirtium-Testing'})
     MockRecorder.set_response_header_removals({'Set-Cookie: AWSALB', 'X-', "Date:"})
@@ -25,31 +25,31 @@ class TestRecordingClimateApi(TestClimateApi):
         self.climateApi = ClimateApi(self.servirtium_site)
 
     def test_averageRainfallForFranceFrom1980to1999Exists(self):
-        MockRecorder.RecorderHttpHandler.set_invoking_method(inspect.stack()[0][3])
+        MockRecorder.RecorderHttpHandler.set_markdown_filename(inspect.stack()[0][3])
         assert self.climateApi.getAveAnnualRainfall(1980, 1999, "fra") == 913.7986955122727
 
     def test_averageRainfallForGreatBritainFrom1980to1999Exists(self):
-        MockRecorder.RecorderHttpHandler.set_invoking_method(inspect.stack()[0][3])
+        MockRecorder.RecorderHttpHandler.set_markdown_filename(inspect.stack()[0][3])
         assert self.climateApi.getAveAnnualRainfall(1980, 1999, "gbr") == 988.8454972331015
 
 
     def test_averageRainfallForEgyptFrom1980to1999Exists(self):
-        MockRecorder.RecorderHttpHandler.set_invoking_method(inspect.stack()[0][3])
+        MockRecorder.RecorderHttpHandler.set_markdown_filename(inspect.stack()[0][3])
         assert self.climateApi.getAveAnnualRainfall(1980, 1999, "egy") == 54.58587712129825
 
     def test_averageRainfallForGreatBritainFrom1985to1995DoesNotExist(self):
-        MockRecorder.RecorderHttpHandler.set_invoking_method(inspect.stack()[0][3])
+        MockRecorder.RecorderHttpHandler.set_markdown_filename(inspect.stack()[0][3])
         with pytest.raises(AttributeError) as execinfo:
             self.climateApi.getAveAnnualRainfall(1985, 1995, "gbr")
         assert "date range 1985-1995 not supported" in execinfo.value.args[0]
 
     def test_averageRainfallForMiddleEarthFrom1980to1999DoesNotExist(self):
-        MockRecorder.RecorderHttpHandler.set_invoking_method(inspect.stack()[0][3])
+        MockRecorder.RecorderHttpHandler.set_markdown_filename(inspect.stack()[0][3])
         with pytest.raises(AttributeError) as excinfo:
             self.climateApi.getAveAnnualRainfall(1980, 1999, "mde")
         assert "not recognized by climateweb" in excinfo.value.args[0]
 
     def test_averageRainfallForGreatBritainAndFranceFrom1980to1999CanBeCalculatedFromTwoRequests(self):
-        MockRecorder.RecorderHttpHandler.set_invoking_method(inspect.stack()[0][3])
+        MockRecorder.RecorderHttpHandler.set_markdown_filename(inspect.stack()[0][3])
         rainfall = self.climateApi.getAveAnnualRainfall(1980, 1999, "gbr", "fra")
         assert rainfall == 951.3220963726872
